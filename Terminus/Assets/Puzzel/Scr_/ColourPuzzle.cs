4fddb1778840aea09e71fe5ebd64c5f55e4de5e3
BarnyIsPurple <99257812+BarnyIsPurple@users.noreply.github.com>
2022-02-11 13:58:57 +0100

    ButtonChangePuzzle

---
+++ Terminus/Assets/Puzzel/Scr_/ColourPuzzle.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourPuzzle : MonoBehaviour
+{
+    public List<Color> colorList = new List<Color>();
+    public List<Color> backupColors = new List<Color>();
+    public List<Renderer> toColor = new List<Renderer>();
+
+
+    private void Start()
+    {
+        for (int i = 0; i < colorList.Count; i++)
+        {
+            backupColors.Add(colorList[i]);
+        }
+    }
+    public void Coret()
+    {
+        SetColors();
+        colorList.Clear(); // Just to be sure.
+        for (int i = 0; i < backupColors.Count; i++)
+        {
+            colorList.Add(backupColors[i]);
+        } 
+    }
+    
+
+    void SetColors()
+    {
+        for (int i = 0; i < toColor.Count; i++)
+        {
+            toColor[i].material.SetColor("_EmissionColor", SetNewColor());
+        }
+    }
+
+    public Color SetNewColor()
+    {
+        int r = Random.Range(0, colorList.Count);
+        Color pickedColor = colorList[r];
+        colorList.RemoveAt(r);
+        return pickedColor;
+    }
+    public void Kanker()
+    {
+        Coret();
+    }
+}
