8e2f44ef85347f5742c747d7d0b9b46c6ff23d80
Gerrielofo <gerlof.reinink@gmail.com>
2022-03-18 17:53:55 +0100

    DoorAnimation
    Beterder + tekst erbij

--- Terminus/Assets/Ziekenhuis/Scripts/DoorOpen.cs
+++ Terminus/Assets/Ziekenhuis/Scripts/DoorOpen.cs
@@ -9,6 +9,8 @@ public class DoorOpen : MonoBehaviour
     public RaycastHit hit;
     public Camera fpsCam;
     public float range = 100f;
+
+    public bool isOpen;
     // Start is called before the first frame update
     void Awake()
     {
@@ -23,26 +25,58 @@ public class DoorOpen : MonoBehaviour
         {
             if (hit.transform.tag == "IsDoor")
             {
-                if (Input.GetKey(KeyCode.E))
+                if(Input.GetKey(KeyCode.E))
                 {
-                    OpenDoor();
+                    if (isOpen == false)
+                    {                    
+                        Animator anim = gameObject.GetComponent<Animator>();
+                        anim.SetFloat("Heehee", -1);
+                        anim.Play("DoorAnim", -1, float.NegativeInfinity);
+                        anim.Play("Door2Anim", -1, float.NegativeInfinity);
+                        isOpen = true;
+                    }
+                    else
+                    {
+                        Animator anim = gameObject.GetComponent<Animator>();
+                        anim.SetFloat("Heehee", 1);
+                        anim.Play("DoorAnim", 1, float.NegativeInfinity);
+                        anim.Play("Door2Anim", 1, float.NegativeInfinity);
+                        isOpen = false;
+                    }
                 }
+                    
             }
         }
-       
-    }
-    void OpenDoor()
-    {
-        
+        //if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            anim.SetTrigger("Open");
+           //if (hit.transform.tag == "IsDoor")
+            {
+                //if (isOpen == true)
+                {
+                    //if (Input.GetKey(KeyCode.E))
+                    {
+                        //Animator anim = gameObject.GetComponent<Animator>();
+                        //anim.SetFloat("Heehee", 1);
+                        //anim.Play("DoorAnim", 1, float.NegativeInfinity);
+                        //anim.Play("Door2Anim", 1, float.NegativeInfinity);
+                        //isOpen = false;
+                    }
+                }
+                
+            }
         }
     }
     void OnGUI()
     {
-       // GUI.enabled = true;
-        //Camera cam = Camera.current;
-        //Vector3 pos = cam.WorldToScreenPoint(new Vector3(0, 5, 0));
-       // GUI.Label(new Rect(pos.x, Screen.height - pos.y, 150, 130), "Press 'E' to open");
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            if (isOpen == false)
+            {
+                if (hit.transform.tag == "IsDoor")
+                {
+                    GUI.Label(new Rect(Screen.width / 2 - 75, Screen.height - 400, 150, 30), "Press 'E' to open the door");
+                }
+            }
+        }
     }
 }
