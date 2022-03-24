f263c22be5a4e34a93f28869bcf4609c8ceb8312
meeskees99 <97054368@st.deltion.nl>
2022-03-24 15:25:41 +0100

    stamina

--- Terminus/Assets/FirstPersonController/Scripts/StaminaController.cs
+++ Terminus/Assets/FirstPersonController/Scripts/StaminaController.cs
@@ -1,8 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StaminaController : MonoBehaviour
 {
- 
+    public float Stamina;
+    float maxStamina;
+
+    public Slider StaminaBar;
+    public float staminaDrain;
+    public float staminaGain;
+
+    void Start()
+    {
+        maxStamina = Stamina;
+        StaminaBar.maxValue = maxStamina;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.W)))
+            DecreaseEnergy();
+        else if (Stamina <= maxStamina)
+            IncreaseEnergy();
+
+        StaminaBar.value = Stamina;
+    }
+
+    public void DecreaseEnergy()
+    {
+        if (Stamina != 0)
+            Stamina -= staminaDrain * Time.deltaTime;       
+    }
+
+    public void IncreaseEnergy()
+    {
+            Stamina += staminaGain * Time.deltaTime;
+    }
 }
