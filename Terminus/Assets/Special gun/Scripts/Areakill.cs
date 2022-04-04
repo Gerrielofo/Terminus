862971ce8e533ba88ad243ec37e1630f805e33f8
Gerrielofo <gerlof.reinink@gmail.com>
2022-04-04 16:45:33 +0200

    GunFixMainScene
    Radius en Weapon UI gefixt

--- Terminus/Assets/Special gun/Scripts/Areakill.cs
+++ Terminus/Assets/Special gun/Scripts/Areakill.cs
@@ -6,9 +6,10 @@ public class Areakill : MonoBehaviour
 {
     public float radius = 10f;
     public float damage = 10f;
+    public Camera fpsCam;
     private void FixedUpdate()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        Collider[] colliders = Physics.OverlapSphere(fpsCam.transform.position, radius);
      foreach(Collider nearbyObject in colliders)
         {
             Target target = nearbyObject.GetComponent<Target>();
