64ec1d16196bc0fa97b04a9b65c6315d0236f2c1
BarnyIsPurple <99257812+BarnyIsPurple@users.noreply.github.com>
2022-04-07 19:40:56 +0200

    Light Puzzle Finished

---
+++ Terminus/Assets/Puzzel/Scr_/Win.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            print("Opened");
+        }
+    }
+
+
+}
+
+
+
